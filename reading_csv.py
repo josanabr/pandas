85f7e92d84de3d6f99032a3503b19b77c4f2a7e4
John Sanabria <john.sanabria@gmail.com>
2018-10-20 13:09:59 +0000

    adicionando operaciones de pandas

--- reading_csv.py
+++ reading_csv.py
@@ -15,3 +15,14 @@ print("Informacion del data frame")
 print(c.info())
 end = time.time()
 print("Tiempo en segundos: %s"%(end - start))
+list(c) # imprime las etiquetas de las columnas
+x = c[['age','capital_gain','hours_per_week']] # retr. particular cols
+x.count() # arroja agregados
+x.sum() # arroja la suma de todos los registros
+x[x.age <= 40] # muestra solo los registros con age <= 40
+x[x.age <= 40].count() # cuenta el numero de records y edad <= 40
+x.age[ x.age <= 40 ] # lo mismo que lo anterior 
+x[x.age <= 40].capital_gain.sum() # record <= 40, y suma capital_gain
+x[x.age <= 40][['capital_gain']].sum() # lo mismo que el anterior
+# otros agregados, min, max, mean, median
+
